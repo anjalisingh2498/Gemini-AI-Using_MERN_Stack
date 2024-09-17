4ca684bd6ff6b3482aff52332ab985616a20e339
ANJALI SINGH <109875199+anjalisingh2498@users.noreply.github.com>
2024-09-17 21:23:44 +0530

    Update NewChatGemini.js

--- public/src/components/NewChat/ScrollChat/NewChatGemini.js
+++ public/src/components/NewChat/ScrollChat/NewChatGemini.js
@@ -4,6 +4,7 @@ import hljs from "highlight.js";
 import "highlight.js/styles/github.css";
 import "./ScrollChatModule.css";
 
+
 const NewChatByGemini = (props) => {
   useEffect(() => {
     hljs.highlightAll();
